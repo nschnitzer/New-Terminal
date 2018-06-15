79b705806c5800a2dc3624ef8f219917a298fd70
nschnitzer <nathanschnitzer587@gmail.com>
2018-06-15 13:08:48 -0400

    Initial Commit

---
+++ Terminal1/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLogOut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShutDown = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTransaction = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuCancel = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lstItems = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuTransaction})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1282, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuLogOut, Me.mnuShutDown})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(44, 24)
+        Me.mnuFile.Text = "File"
+        '
+        'mnuLogOut
+        '
+        Me.mnuLogOut.Name = "mnuLogOut"
+        Me.mnuLogOut.Size = New System.Drawing.Size(216, 26)
+        Me.mnuLogOut.Text = "Log Out"
+        '
+        'mnuShutDown
+        '
+        Me.mnuShutDown.Name = "mnuShutDown"
+        Me.mnuShutDown.Size = New System.Drawing.Size(216, 26)
+        Me.mnuShutDown.Text = "Shut Down"
+        '
+        'mnuTransaction
+        '
+        Me.mnuTransaction.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuCancel})
+        Me.mnuTransaction.Name = "mnuTransaction"
+        Me.mnuTransaction.Size = New System.Drawing.Size(96, 24)
+        Me.mnuTransaction.Text = "Transaction"
+        '
+        'mnuCancel
+        '
+        Me.mnuCancel.Name = "mnuCancel"
+        Me.mnuCancel.Size = New System.Drawing.Size(216, 26)
+        Me.mnuCancel.Text = "Cancel"
+        '
+        'lstItems
+        '
+        Me.lstItems.FormattingEnabled = True
+        Me.lstItems.ItemHeight = 16
+        Me.lstItems.Location = New System.Drawing.Point(39, 55)
+        Me.lstItems.MultiColumn = True
+        Me.lstItems.Name = "lstItems"
+        Me.lstItems.Size = New System.Drawing.Size(1154, 452)
+        Me.lstItems.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1282, 624)
+        Me.Controls.Add(Me.lstItems)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuLogOut As ToolStripMenuItem
+    Friend WithEvents mnuShutDown As ToolStripMenuItem
+    Friend WithEvents mnuTransaction As ToolStripMenuItem
+    Friend WithEvents mnuCancel As ToolStripMenuItem
+    Friend WithEvents lstItems As ListBox
+End Class
